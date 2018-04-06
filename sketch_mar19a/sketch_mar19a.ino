a00a0d6dbebfddaeff1f43129cde750f084d7fc3
gergopc <gergoesjanka@gmail.com>
2018-04-06 12:56:46 +0300

    arduino lcd

--- sketch_mar19a/sketch_mar19a.ino
+++ sketch_mar19a/sketch_mar19a.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
 
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.println("Hello");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+ 
 }
+
